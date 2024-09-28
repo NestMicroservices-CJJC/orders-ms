928576e0215fb2761840191f46244717f57e76e3
Christian John jiménez Calvo <christianjjc@mail.com>
2024-09-27 19:06:12 -0500

    sect06-ep04-configurar-orders-ms

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,34 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { envs } from './config';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
-async function bootstrap() {
+/* async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const logger = new Logger('OrdersMS-main');
   await app.listen(envs.port);
   logger.log(`App Running on port ${envs.port}`);
+}
+bootstrap(); */
+
+//* CON MICRO SERVICIOS
+async function bootstrap() {
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+    transport: Transport.TCP,
+    options: {
+      port: envs.port,
+    },
+  });
+  const logger = new Logger('OrdersMS-main');
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  await app.listen();
+  logger.log(`Orders Microservices Running on port ${envs.port}`);
 }
 bootstrap();
